8d563445033eab658266dc903695c4942d1b91c5
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2021-05-26 22:34:19 +0800

    optimize(springSession): update session cookie name

--- src/main/java/xyz/kmahyyg/eshopdemo/SessionConfig.java
+++ src/main/java/xyz/kmahyyg/eshopdemo/SessionConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 @EnableRedisHttpSession
@@ -11,5 +13,13 @@ public class SessionConfig {
     @Bean
     public LettuceConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CookieSerializer personCookieSerializer(){
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        return serializer;
     }
 }
